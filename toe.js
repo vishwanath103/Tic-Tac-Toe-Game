79879f6e6800adf5a87fd2acf79cff4c39e0a5a2
Vishwanath Acharya <bailorevishwanath02@gmail.com>
2016-03-12 19:00:57 +0530

    Running MiniMax Algorithm

--- toe.js
+++ toe.js
@@ -1,65 +1,19 @@
-// var painted = [];
-// var content = [];
-// var theCanvas,c,cxt;
-// var turn = 0;
-// var squaresFilled;
-// var winningCombinations = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
-// $(document).ready(function(){
-// 	for (var i = 0; i < 9; i++) {
-// 		painted[i] = false;
-// 		content[i] = "";
-// 	}		
-// });
-
-// function canvasClicked(num) {
-// 	theCanvas = "canvas" + num;
-// 	c = document.getElementById(theCanvas);
-
-// 	if(painted[num-1] == false){
-// 		if(turn%2 == 0){
-// 			$('#'+theCanvas).html("X");
-// 			content[num-1] = 'X';
-// 		}
-// 		else{
-// 			$('#'+theCanvas).html("O");
-// 			content[num-1] = 'O';
-// 		}
-// 		turn++;
-// 		painted[num-1] = true;
-// 		squaresFilled++;
-// 		checkForWinners(content[num-1]);
-// 		if(squaresFilled == 9){
-// 			alert("Game Over");
-// 			location.reload();
-// 		}
-// 	}
-// }
-
-// function checkForWinners(symbol) {
-// 	for (var i = 0; i < winningCombinations.length; i++) {
-// 		if(content[winningCombinations[i][0]]==symbol&&content[winningCombinations[i][1]]== symbol&&content[winningCombinations[i][2]]==symbol){
-//         alert(symbol+ " WON!");
-//         location.reload();
-// 	}
-// }
-// }
-
-// function compTurn()
-
 
 //turn=0---> Comp Turn
 //turn=1---> Player Turn
 var turn=0;
 var theCanvas;
-var state = [];
+var state = new Array(10);
 var tree = [];
-var winningCombinations = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+var winningCombinations = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]];
+var squaresFilled = 0;
 $(document).ready(function(){
 	turn = 1;
 	var random = Math.round(Math.random() * 8 + 1);
 	state[random] = 'X';
 	theCanvas = "canvas" + random;
 	$('#'+theCanvas).html("X");
+	squaresFilled++;
 });
 
 function canvasClicked(num) {
@@ -72,13 +26,27 @@ function canvasClicked(num) {
 	    	compTurn();
 		}
 	}
+	squaresFilled++;
+	hasWon('O');
+	if(squaresFilled == 9){
+		alert("Draw");
+		location.reload();
+	}
 }
 
 function compTurn(){
 	tree = state;
 	var arr = miniMax(2,0);
-	alert(arr);
+	theCanvas = "canvas" + arr[1];
+	$('#'+theCanvas).html("X");
+	state[arr[1]] = "X";
 	turn = 1;
+	squaresFilled++;
+	hasWon('X');
+	if(squaresFilled == 9){
+		alert("Draw");
+		location.reload();
+	}
 }
 
 function miniMax(depth,turn){
@@ -97,20 +65,20 @@ function miniMax(depth,turn){
 		nextMoves.forEach(function(move){
 			tree[move] = (turn == 0)?'X':'O';
 			if(turn == 0){  //Computer
-				currentScore = miniMax(depth-1,1-turn);
-				alert(currentScore);
+				currentScore = miniMax(depth-1,1-turn)[0];
 				if(currentScore>bestScore){
 					bestScore = currentScore;
 					bestMove = move;
 				}
 			}
 			else{ //Player
-				currentScore = miniMax(depth-1,1-turn);
+				currentScore = miniMax(depth-1,1-turn)[0];
 				if(currentScore<bestScore){
 					bestScore = currentScore;
 					bestMove = move;
 				}
 			}
+			tree[move] = null;
 		});
 	}
 	return [bestScore,bestMove];
@@ -174,4 +142,13 @@ function evaluateLine(a,b,c) {
 	}
 	return score;
 
+}
+
+function hasWon(symbol) {
+	for (var i = 0; i < winningCombinations.length; i++) {
+	if(state[winningCombinations[i][0]]==symbol&&state[winningCombinations[i][1]]== symbol&&state[winningCombinations[i][2]]==symbol){
+        alert(symbol+ " WON!");
+         location.reload();
+ 	}
+ }
 }
